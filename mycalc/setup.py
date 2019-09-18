2ccbc00d94d34b4970a340ceaa6873d7c1c2e661
mt <breddowen@gmail.com>
2019-09-18 23:12:34 +0300

    Add setup.py

---
+++ mycalc/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+	name="mycalc",
+	version="0.0.1",
+	description="A small package example with some ariphmetics",
+	packages=find_packages(),
+	includa_package_data=True,
+	author="Name Surname",
+	author_email="author@example.com",	
+	url="https://github.com/MaxTitkov/my-project",
+	)
